15f11b9e66cf5980c225f60e9a65fd5547ba71d5
Peretyaka <peretyaka@gmail.com>
2012-04-06 17:10:37 +0300

    Merge develop 2

--- modules/cms3/auth/classes/model/user.php
+++ modules/cms3/auth/classes/model/user.php
@@ -14,7 +14,7 @@ class Model_User extends Model implements \Acl_Role_Interface {
 		$meta->fields(array(
 			'id'		=> ORM::field('primary'),
 			'enabled'	=> ORM::field('boolean'),
-			'name'		=> ORM::field('string', array(
+			'display_name'		=> ORM::field('string', array(
 				'column' => 'display_name',
 				'show_in_list' => true
 			))
